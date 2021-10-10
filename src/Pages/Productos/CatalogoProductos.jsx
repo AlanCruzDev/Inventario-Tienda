b9a3b0354d24bb3974e062667263cbf4e3db2d10
AlanCruzDev <alancruzv50@gmail.com>
2021-10-10 14:09:29 -0500

    cambios en catalogo productos

--- src/Pages/Productos/CatalogoProductos.jsx
+++ src/Pages/Productos/CatalogoProductos.jsx
@@ -1,7 +1,70 @@
 import React, { useEffect } from "react";
+import {useHistory} from 'react-router-dom';
+import {Table} from '../../Components/Table/table';
+import { useDispatch, useSelector } from "react-redux";
+import {ErrorToken} from '../../Action/auth.action';
+import {ListarProductos,LimpiarVariables} 
+from '../../Action/productos.action';
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 export const CatalogoProductos = () => {
-  useEffect(() => {}, []);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  
+  const { auth, producto } = useSelector((state) => state);
+
+  const { dateUser } = auth;
+  const { idTienda } = dateUser[0].results[0];
+
+  const { listadoProducto } = producto;
+  const data = !!listadoProducto && listadoProducto.results;
+
+  
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (!token) {
+      dispatch(ErrorToken());
+      history.push("/auth");
+    }
+  }, []);
+
+  useEffect(() => {
+    dispatch(ListarProductos(idTienda));
+    return () => {
+      dispatch(LimpiarVariables());
+    };
+  }, []);
+
+  const Header = () => {
+    return (
+      <tr>
+        <th>Nombre Producto</th>
+        <th>Precio</th>
+        <th>Cantidad</th>
+        <th>Nombre Proveedor</th>
+        <th>Nombre Categoria</th>
+      </tr>
+    );
+  };
+  const Content = () => {
+    const content = data.map((valor) => {
+      return (
+        <tr key={valor.idProducto}>
+          <td>{valor.NombreProducto}</td>
+          <td>{valor.Precio}</td>
+          <td>{valor.Cantidad}</td>
+          <td>{valor.NombreProveedor}</td>
+          <td>{valor.NombreCategoria}</td>
+        </tr>
+      );
+    });
+    return content;
+  };
+  const TablaDibujar = () => {
+    return <Table Header={Header} Content={Content} idnombre="tablaProducto" />;
+  };
 
   return (
     <main>
@@ -11,11 +74,21 @@ export const CatalogoProductos = () => {
         </div>
         <div className="estadisticas__estudios">
           <div className="card-header">
-            <button className="btn btn-primary">d</button>
-
+            <ul className="nav card-header-tabs ml-auto">
+              <li className="d-inline p-2">
+                <ReactHTMLTableToExcel
+                  id="tablaProductos"
+                  className="btn btn-secondary"
+                  table="tablaProducto"
+                  filename="listaProductosxls"
+                  sheet="Productosjajajajaxls"
+                  buttonText="Descargar Exel"
+                />
+              </li>
+            </ul>
           </div>
           <div className="card__estadisticas">
-
+            {data.length > 0 ? TablaDibujar() : null}
           </div>
         </div>
       </div>
